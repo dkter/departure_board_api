4ef8fed2b2897f65a8254decb26aefda07dc9084
David Teresi <dkteresi@gmail.com>
2025-07-26 17:10:52 -0400

    Serve on 0.0.0.0

--- departure_server/src/main.rs
+++ departure_server/src/main.rs
@@ -151,7 +151,7 @@ async fn main() -> anyhow::Result<()> {
                 }))
                 .service(get_departures)
         })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await?)
 }
