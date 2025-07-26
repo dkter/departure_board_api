36034500775d14b8a1a85b235e15455629ce2855
David Teresi <dkteresi@gmail.com>
2025-07-26 18:02:25 -0400

    Add endpoint that you can pass in the time to

--- departure_server/src/main.rs
+++ departure_server/src/main.rs
@@ -126,6 +126,42 @@ async fn get_departures(path: web::Path<(f64, f64, u64)>, data: web::Data<AppSta
     Ok(web::Json(formatted_data))
 }
 
+#[get("/departures-after-time/{time}/{lat}/{lon}/{limit}")]
+async fn get_departures_after_time(path: web::Path<(u32, f64, f64, u64)>, data: web::Data<AppState>) -> error::Result<web::Json<Vec<FormattedData>>> {
+    let db_client = data.pool.get().await?;
+    let (time, lat, lon, limit) = path.into_inner();
+    let now = gtfs::GtfsTime::from(time);
+
+    let result = get_next_deps_near_point().bind(&db_client, &lat, &lon, &(limit as i64), &now.into()).all().await?;
+
+    // collect agencies to get updates from
+    let agencies = result.iter().map(|r| &r.agency).unique();
+    // map of {agency name: realtime updates for that agency}
+    let updates = agencies.map(async |a| (a, get_agency_updates(&data.cfg, a, &data.req_client).await))
+        .collect::<FuturesUnordered<_>>()
+        .filter_map(async |(agency_name, agency_updates)| {
+            // if there's an error, just log and don't include updates for that agency
+            match agency_updates {
+                Ok(agency_updates) => Some((agency_name, agency_updates)),
+                Err(e) => {
+                    println!("warning: could not retrieve updates for agency {} due to error: {}", agency_name, e);
+                    None
+                }
+            }
+        })
+        .collect::<HashMap<_, _>>()
+        .await;
+
+    let mut formatted_data = result.iter().map(|r| {
+        let f = formatter::get_formatter_from_agency(&r.agency);
+        f.format(r)
+    }).collect::<Vec<_>>();
+
+    apply_updates_to_formatted_data_list(&now, &updates, &mut formatted_data);
+
+    Ok(web::Json(formatted_data))
+}
+
 async fn create_pool() -> Result<Pool, CreatePoolError> {
     let mut cfg = Config::new();
     cfg.user = Some(String::from("departure_board"));
